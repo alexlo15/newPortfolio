292d5dc71b0cc52a77c4e05f334b1c57a0af8b56
Alex <loalexander389@gmail.com>
2019-12-10 12:30:53 -0500

    css and title

--- src/Components/Ticker/Ticker.js
+++ src/Components/Ticker/Ticker.js
@@ -5,7 +5,15 @@ class Ticker extends Component {
     render() {
         return (
             <>
-                <p className="marquee"><span> 732-403-6636  loalexander389@gmail.com   </span></p>
+                <p className="marquee">
+                    <span>
+                        &lt; 732-403-6636  &gt; &nbsp;
+                        &lt; loalexander389@gmail.com &gt; &nbsp;
+                        &lt; <a href="https://docs.google.com/document/d/1eJDtlLm0e73J-ZIg6oOMsPVtNewKBPIrFBTJO02Wbkg/edit?usp=sharing" target="_blank">
+                            Resume</a>
+                        &gt;
+                     </span>
+                </p>
             </>
         );
     }
